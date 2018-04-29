02876caada04d8d21e41d0b55ef3e7b0fed4c1d1
Peter McGrath <mcgrathprj@gmail.com>
2018-04-29 17:39:28 -0400

    modularized

--- server.js
+++ server.js
@@ -2,19 +2,18 @@
 const express = require('express');
 const router = express.Router();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
-
-const jsonParser = bodyParser.json();
 const app = express();
 
+const blogPostsRouter = require('./blogPostsRouter');
+
 app.use(morgan('common'));
+app.use(express.static('public'));
 
-BlogPosts.create("Aggretsuko Is All Of Us And We Are All Aggretsuko","Some content about Aggretsuko", "Charles Pulliam-Moore", )
-BlogPosts.create("Roomful of Rich, White NYC Parents Get Big Mad at Plan to Diversify Neighborhood's Schools", "Yikes. Just yikes.", "Anne Branigin")
-BlogPosts.create("Tesla Driver Banned From Highway After Being Caught In the Passenger Seat", "Yikes again.", "Elizabeth Werth")
+app.use('/blog-posts', blogPostsRouter);
 
-app.get('/blog-posts', (req, res) => {
-  res.json(BlogPosts.get());
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
