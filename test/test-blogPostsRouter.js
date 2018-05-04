bd5ae14c4f9a0de8b8dbece73270b8b3c037f341
Peter McGrath <mcgrathprj@gmail.com>
2018-05-04 19:22:27 -0400

    initial commit for tests

---
+++ test/test-blogPostsRouter.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+// this lets us use *expect* style syntax in our tests
+// so we can do things like `expect(1 + 1).to.equal(2);`
+// http://chaijs.com/api/bdd/
+const expect = chai.expect;
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
+
+
+describe('Blog Posts', function() {
+
+  // Before our tests run, we activate the server. Our `runServer`
+  // function returns a promise, and we return the that promise by
+  // doing `return runServer`. If we didn't return a promise here,
+  // there's a possibility of a race condition where our tests start
+  // running before our server has started.
+  before(function() {
+    return runServer();
+  });
