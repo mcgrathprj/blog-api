c06ed77965f02fc378f2285d967898ee266dea37
Peter McGrath <mcgrathprj@gmail.com>
2018-05-05 16:27:31 -0400

    cleaned up extra server starts

--- server.js
+++ server.js
@@ -37,8 +37,4 @@ function closeServer() {
   });
 }
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
-
 module.exports = {app, runServer, closeServer};
--- test/test-server.js
+++ test/test-server.js
@@ -71,10 +71,7 @@ describe('Blog Posts', function() {
       })
 
       .then(function(res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.deep.equal(updateData);
+        expect(res).to.have.status(204);
       });
   });
 
