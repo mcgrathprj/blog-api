9bfd79527d1c77cd4760ff4e25a8202e143a873f
Peter McGrath <mcgrathprj@gmail.com>
2018-05-05 07:35:48 -0400

    more test cleanup

--- server.js
+++ server.js
@@ -12,6 +12,32 @@ app.use(express.static('public'));
 
 app.use('/blog-posts', blogPostsRouter);
 
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
